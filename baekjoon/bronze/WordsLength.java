a78ce1015d6e1f06dcc4033f0d81fdc984a5f129
mni-js <2ujs4311@gmail.com>
2025-01-21 16:29:58 +0900

    2743-단어 길이 재기 문제 해결

---
+++ baekjoon/bronze/WordsLength.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class WordsLength {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = bf.readLine();
+        System.out.println(word.length());
+    }
+}
