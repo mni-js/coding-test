52a3d34003f1121b745bc0114d584834c6fe27c2
mni-js <2ujs4311@gmail.com>
2025-01-21 16:28:00 +0900

    27866-문자와 문자열 문제 해결

---
+++ baekjoon/bronze/CharString.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class CharString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input_word = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.println(input_word.charAt(i-1));
+    }
+}
