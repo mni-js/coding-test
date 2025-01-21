2f87ee23b92df3103826037eb70e2d7e8a3c9282
mni-js <2ujs4311@gmail.com>
2025-01-21 16:50:58 +0900

    11654-아스키코드 문제 해결

---
+++ baekjoon/bronze/StringToAscii.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze;
+
+import java.io.*;
+
+public class StringToAscii {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int input = bf.read();
+        System.out.println(input);
+    }
+}
