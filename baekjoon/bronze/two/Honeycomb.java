00b443922e71a51d482ba0166d8038edc81782ee
jse <2ujs4311@gmail.com>
2025-02-09 21:58:35 +0900

    2292-벌집 문제 해결

---
+++ baekjoon/bronze/two/Honeycomb.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Honeycomb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int layer = 1;
+        int startNum = 1;
+
+        while (N > startNum) {
+            layer++;
+            startNum += ((layer - 1) * 6);
+        }
+
+        System.out.println(layer);
+    }
+}
