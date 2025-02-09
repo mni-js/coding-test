9cc93c5075c816677322872885e39e3ec2c6e7a7
jse <2ujs4311@gmail.com>
2025-02-09 21:40:55 +0900

    2903-중앙이동알고리즘 문제 해결

---
+++ baekjoon/bronze/three/CentralMovingAlgorithm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class CentralMovingAlgorithm {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int initialDotCountRow = 2;
+
+        for (int i = 0; i < N; i++) {
+            initialDotCountRow += (initialDotCountRow - 1);
+        }
+
+        int numOfDot = initialDotCountRow * initialDotCountRow;
+        System.out.println(numOfDot);
+    }
+}
