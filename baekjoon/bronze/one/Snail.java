b08aa6539bbd0e96f629b0ad942d51bb455b1dbb
jse <2ujs4311@gmail.com>
2025-02-10 22:18:49 +0900

    2869-달팽이는 올라가고 싶다 문제 해결

---
+++ baekjoon/bronze/one/Snail.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Snail {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] ABV = reader.readLine().split(" ");
+
+        int A = Integer.parseInt(ABV[0]);
+        int B = Integer.parseInt(ABV[1]);
+        int V = Integer.parseInt(ABV[2]);
+
+        int day = (int) Math.ceil((double) (V - A) / (A - B)) + 1;
+        System.out.println(day);
+    }
+}
