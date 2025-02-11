5777a3c5c19bb4a59698a93da9c5ddb0e64878fc
jse <2ujs4311@gmail.com>
2025-02-11 15:01:21 +0900

    1978-소수 찾기 문제 해결

---
+++ baekjoon/bronze/two/FindPrime.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+
+class FindPrime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+        int[] numberArr = Arrays.stream(reader.readLine().split(" "))
+                                .mapToInt(Integer::parseInt)
+                                .toArray();
+        int primeCnt = 0;
+        
+        for (int n: numberArr) {
+            if (n == 1) {
+                continue;
+            }
+            int factorsCnt = 0;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    factorsCnt++;
+                }
+            }
+
+            if (factorsCnt == 0) {
+                primeCnt++;
+            }
+        }
+        System.out.println(primeCnt);
+    }
+}
