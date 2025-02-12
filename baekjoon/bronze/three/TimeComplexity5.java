35d91c9a6d5e6d26f8102487d47b5037f9353b1e
jse <2ujs4311@gmail.com>
2025-02-12 11:13:07 +0900

    24266-알고리즘의 수행 시간 5 문제 해결

---
+++ baekjoon/bronze/three/TimeComplexity5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class TimeComplexity5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int degree = 3;
+        long result = 1L;
+        for (int i = 0; i < degree; i++) {
+            result *= n;
+        }
+
+        System.out.println(result);
+        System.out.println(degree);
+    }
+}
