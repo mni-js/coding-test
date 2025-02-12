d43684f8e270b55cde415f5df8375c1d3ad90058
jse <2ujs4311@gmail.com>
2025-02-12 10:57:22 +0900

    24265-알고리즘의 수행 시간 4 문제 해결

---
+++ baekjoon/bronze/three/TimeComplexity4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class TimeComplexity4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long result = 0;
+
+        for (int i = 1; i < n; i++) {
+            result += i;
+        }
+        System.out.println(result);
+        System.out.println(2);
+    }
+}
