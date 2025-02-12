3bcd24cd8f594dee2d2c46e2cf47edff936097ca
jse <2ujs4311@gmail.com>
2025-02-12 10:47:25 +0900

    24264-알고리즘의 수행 시간 3 문제 해결

---
+++ baekjoon/bronze/three/TimeComplexity3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class TimeComplexity3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println((long) Math.pow(n, 2));
+        System.out.println(2);
+    }
+}
