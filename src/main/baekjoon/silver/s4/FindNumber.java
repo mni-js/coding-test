b68c17fdfcc4718a79aa7dbc9c4ce62dade01439
mni-js <2ujs4311@gmail.com>
2025-03-25 19:21:27 +0900

    1920-수 찾기 문제 해결

--- src/main/baekjoon/silver/s4/FindNumber.java
+++ src/main/baekjoon/silver/s4/FindNumber.java
@@ -1,7 +1,8 @@
-package baekjoon.silver.s4;
+package silver.s4;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class FindNumber {
     public static void main(String[] args) throws IOException {
@@ -9,25 +10,32 @@ public class FindNumber {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(reader.readLine());
-        int[] targetNums = Arrays.stream(reader.readLine().split(" "))
-                                 .mapToInt(Integer::parseInt)
-                                 .toArray();
-        int M = Integer.parseInt(reader.readLine());
-        int[] numsToCheck = Arrays.stream(reader.readLine().split(" "))
-                                  .mapToInt(Integer::parseInt)
-                                  .toArray();
-        reader.close();
+        int[] targetNums = new int[N];
 
-        depth1: for (int checkNum : numsToCheck) {
-            depth2: for (int target : targetNums) {
-                if (checkNum == target) {
-                    writer.write(1 + "\n");
-                    continue depth1;
-                }
+        StringTokenizer targetTokenizer = new StringTokenizer(reader.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            targetNums[i] = Integer.parseInt(targetTokenizer.nextToken());
+        }
+        Arrays.sort(targetNums);
+
+        int M = Integer.parseInt(reader.readLine());
+        int[] numsToCheck = new int[M];
+
+        StringTokenizer checkTokenizer = new StringTokenizer(reader.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            numsToCheck[i] = Integer.parseInt(checkTokenizer.nextToken());
+        }
+
+        for (int num : numsToCheck) {
+            if (Arrays.binarySearch(targetNums, num) < 0) {
+                writer.write(0 + "\n");
+            } else {
+                writer.write(1 + "\n");
             }
-            writer.write(0 + "\n");
         }
         writer.flush();
+
+        reader.close();
         writer.close();
     }
 }
