d99d13f399cf65dc04c68f5fda65c32484233821
mni-js <2ujs4311@gmail.com>
2025-03-31 21:23:41 +0900

    7568-덩치 문제 해결

--- src/main/baekjoon/silver/s5/Scale.java
+++ src/main/baekjoon/silver/s5/Scale.java
@@ -1,41 +1,44 @@
-package baekjoon.silver.s5;
+package silver.s5;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+/*
+    x kg, y cm -> (x, y)로 표현.
+    (x1, y1), (x2, y2)에서 x1 > x2, y1 > y2일 때만 덩치가 더 크다고 표현함.
+    그 외에는 같은 덩치로 취급함.
+    첫째 줄에는 전체 사람 수 N
+    이어지는 N개의 줄에는 x와 y가 공백 한 칸을 두고 입력된다.
+ */
 public class Scale {
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(reader.readLine());
-        String[][] scaleArray = new String[N][];
+        int[][] weightHeights = new int[N][];
         for (int i = 0; i < N; i++) {
-            scaleArray[i] = reader.readLine().split(" ");
+            int[] weightHeight = Arrays.stream(reader.readLine().split(" "))
+                                       .mapToInt(Integer::parseInt)
+                                       .toArray();
+            weightHeights[i] = weightHeight;
         }
-        reader.close();
 
-        String[][] copyAndSortReverse = Arrays.copyOf(scaleArray, scaleArray.length);
-        Arrays.sort(copyAndSortReverse, (x, y) -> Integer.parseInt(y[0]) - Integer.parseInt(x[0]));
-        System.out.println(Arrays.deepToString(copyAndSortReverse));
-
-        Map<String[], Integer> placeMap = new HashMap<>();
-        int place = 1;
-        int samePlaceCount = 0;
-        int minHeight = 0;
-        for (int i = 0; i < copyAndSortReverse.length; i++) {
-            minHeight = Integer.parseInt(copyAndSortReverse[i][1]);
-            if (Integer.parseInt(copyAndSortReverse[i][1]) >= Integer.parseInt(copyAndSortReverse[i + 1][1])) {
-                placeMap.put(copyAndSortReverse[i], place);
-                place++;
-            } else {
-                samePlaceCount++;
-                placeMap.put(copyAndSortReverse[i], place);
+        for (int i = 0; i < N; i++) {
+            int nthPlace = 1;
+            for (int j = 0; j < N; j++) {
+                if (i != j) {
+                    if ((weightHeights[i][0] < weightHeights[j][0]) && (weightHeights[i][1] < weightHeights[j][1])) {
+                        nthPlace++;
+                    }
+                }
             }
+            writer.write(nthPlace + " ");
         }
+        writer.flush();
 
+        reader.close();
+        writer.close();
     }
 }
